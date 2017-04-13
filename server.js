b80246a227d2fcec1425055fafe992a7e2d49e10
bhavin00 <bhavin.p_productivet@outlook.com>
2017-04-13 20:44:25 +0530

    sdsd

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ var app = express();
 var passport = passport();
 
 //Listening on port 3002
-app.listen(3015);
+app.listen( process.env.PORT || 3002);
 console.log('Server started on http://localhost:3015');
 module.exports = app;
